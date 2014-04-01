78c027074c40a8809e71b3f51cb55ee1f034bd89
Noah Birnel <nbirnel@gmail.com>
2014-04-01 15:25:45 -0700

    nuke for fresh start

--- lib/rscaffold/TEMPLATE/spec/PROJECT_SPEC.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-require  "#{File.dirname(__FILE__)}/../lib/!!PROJECT!!"
-
-describe !!PROJECTCAMEL!! do
-  before do
-  end
-
-end
--- lib/rscaffold/version.rb
+++
@@ -1,9 +0,0 @@
-class Rscaffold
-  module Version
-    MAJOR = 0 
-    MINOR = 0 
-    PATCH = 0 
-
-    [MAJOR, MINOR, PATCH].join('.')
-  end
-end
